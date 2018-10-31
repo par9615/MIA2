682d34a27281b220704c4bee031f6aa28ba1deb0
Miguel Hernández <par9615@gmail.com>
2018-10-31 02:58:53 -0600

    fix: gets the indexes of the features

--- proyecto2.m
+++ proyecto2.m
@@ -6,12 +6,12 @@ data = xlsread('Datos_Proyecto2.xls', 1, 'A1:J313');
 nf = size(data,2)-1; %numero de features
 
 for k=1:2^nf-1  %Itera sobre todos los posibles subconjuntos excepto el vacio
-    indexes = logical(de2bi(k, nf))
-    nfk = sum(indexes)                     %numero de variables en en el subconjunto
-    datasub = [data(:,indexes) data(:,nf+1)] %subconjunto de features
-    cr = corrcoef(datasub)                  %tabla de correlaciones
+    indexes = logical(de2bi(k, nf));        
+    nfk = sum(indexes);                     %numero de variables en en el subconjunto
+    datasub = [data(:,indexes) data(:,nf+1)]; %subconjunto de features
+    cr = corrcoef(datasub);                  %tabla de correlaciones
     
-    rcf = sum(cr(1:nfk,nfk+1)) %correlacion feature-clasificacion
+    rcf = sum(cr(1:nfk,nfk+1)); %correlacion feature-clasificacion
     rff = 0;                     %correlacion feature-feature
     
     for i=1:nfk
@@ -23,6 +23,8 @@ for k=1:2^nf-1  %Itera sobre todos los posibles subconjuntos excepto el vacio
     cfs(k)=rcf/sqrt(nfk+2*rff);
 end
 
+[val, index]=max(cfs);
+features=find(de2bi(index,nf));
 
 
 
