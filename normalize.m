dcf7f4279e5f9bf4f04ccabcd1c2e5d6b736fe02
Miguel Hernández <par9615@gmail.com>
2018-11-01 20:24:16 -0600

    feature selection finished

---
+++ normalize.m
@@ -0,0 +1,10 @@
+function [X] = normalize(data)
+	cols = size(data, 2);
+	rows = size(data, 1);
+	X = zeros(rows, cols);
+	for i=1:cols
+		meanCol = mean(data(:,i));
+		sigma = std(data(:,i));
+        X(:,i) = (data(:,i) - meanCol) / sigma;
+	end
+end
--- proyecto2.m
+++ proyecto2.m
@@ -3,7 +3,10 @@ clear all;
 clc;
 
 data = xlsread('Datos_Proyecto2.xls', 1, 'A1:J313');
+data(:,1:9) = normalize(data(:,1:9));
 nf = size(data,2)-1; %numero de features
+
+meancr=mean(corrcoef(data), 2);  %promedio de correlaciones entre de cada variable
 
 for k=1:2^nf-1  %Itera sobre todos los posibles subconjuntos excepto el vacio
     indexes = logical(de2bi(k, nf));        
@@ -20,11 +23,43 @@ for k=1:2^nf-1  %Itera sobre todos los posibles subconjuntos excepto el vacio
        end
     end
 
-    cfs(k)=rcf/sqrt(nfk+2*rff);
+    cfs(k,:)=[rcf/sqrt(nfk+2*rff) nfk k];
 end
 
-[val, index]=max(cfs);
-features=find(de2bi(index,nf));
+%CFS maximo por numero de features
+
+    maxi=-9999;
+    for j=1:2^nf-1
+        if cfs(j,2)==6
+            if cfs(j,1)>maxi
+               maxi = cfs(j,1);
+               row = cfs(j,:);
+            end
+        end
+    end    
+
+row
+de2bi(row(3))
+
+
+% %CFS promedio por numero de features
+% for i=1:nf
+%     meani(i)=0;
+%     count=0;
+%     for j=1:2^nf-1
+%         if cfs(j,2)==i            
+%             meani(i) = meani(i)+cfs(j,1);
+%             count=count+1;
+%         end
+%     end    
+%     meani(i) = meani(i)/count;
+% end
+% 
+% %plot(meani);
+
+%[val, index]=max(cfs(:,1));
+%features=find(de2bi(index,nf));
+
 
 
 
